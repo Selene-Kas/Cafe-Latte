b4dcd05f337ad0d9c82d67c72b7f3d59902fab47
Kassandra Sanchez <kassandrasmedina@gmail.com>
2024-03-28 15:04:21 -0700

    init function

---
+++ backend/server/index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const { client,
+    createTables
+} = require('./db');
+
+const app = express();
+
+app.use(express.json());
+
+const init = async() => {
+  await client.connect();
+  console.log('connected to databse');
+  await createTables();
+  console.log('tables created');
+
+  app.listen(3000, () => {
+    console.log('server is listening on port 3000!');
+  });
+};
+init();
