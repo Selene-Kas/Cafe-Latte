4e704ca7cc728b988ebef7dca092020678602df2
Kassandra Sanchez <kassandrasmedina@gmail.com>
2024-03-31 21:08:26 -0700

    imports and exports of pages

---
+++ frontend/client/pages/index.js
@@ -0,0 +1,10 @@
+import About from "./About";
+import Home from "./Home";
+import Cart from "./Cart";
+import Details from "./Details";
+import Login from "./Login";
+import Navbar from "./Navbar";
+import Register from "./Register";
+import Footer from "./Footer";
+
+export { About, Home, Cart, Details, Login, Navbar, Register, Footer };
