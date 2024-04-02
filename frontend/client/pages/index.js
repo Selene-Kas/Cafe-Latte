40ab0b94ca449737d3a69f4fc12a08c922d33586
Kassandra Sanchez <kassandrasmedina@gmail.com>
2024-04-02 16:41:45 -0700

    added checkout

--- frontend/client/pages/index.js
+++ frontend/client/pages/index.js
@@ -6,5 +6,6 @@ import Login from "./Login";
 import Navbar from "./Navbar";
 import Register from "./Register";
 import Footer from "./Footer";
+import Checkout from "./Checkout";
 
-export { About, Home, Cart, Details, Login, Navbar, Register, Footer };
+export { About, Home, Cart, Details, Login, Navbar, Register, Footer, Checkout };
