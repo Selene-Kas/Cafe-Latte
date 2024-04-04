6bfb2462492c31805f40d851dec0f87149b6e409
Kassandra Sanchez <kassandrasmedina@gmail.com>
2024-04-04 13:23:40 -0700

    form and labels

--- frontend/client/pages/Login.jsx
+++ frontend/client/pages/Login.jsx
@@ -1,9 +1,20 @@
-import { Link } from 'react-router-dom'
+
 
 const Login = () => {
   return(
     <div>
       <h1>Login</h1>
+      <form>
+        <label>
+            <h2>Email:</h2>
+            <input />
+        </label>
+        <label>
+            <h2>Password:</h2>
+            <input />
+        </label>
+        <button type="submit"> Login </button>
+      </form>
     </div>
   );
 }
