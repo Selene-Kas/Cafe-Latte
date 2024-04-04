56f221fe929978a3d839bfde1a5c29bd2001d83e
Kassandra Sanchez <kassandrasmedina@gmail.com>
2024-04-04 13:21:28 -0700

    useEffect

--- frontend/client/pages/Details.jsx
+++ frontend/client/pages/Details.jsx
@@ -1,9 +1,31 @@
-import { Link } from 'react-router-dom'
+import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import { fetchSingleProduct } from "./api";
 
 const Details = () => {
+  const { id }= useParams();
+  const [product, setProduct] = useState( );
+
+  useEffect(()=>{
+    async function getProductDetails() {
+        const productDetails = await fetchSingleProduct(id);
+       setProduct(productDetails);
+       console.log(productDetails);
+    }
+    getProductDetails();
+  }, [id] )
+  
   return(
     <div>
-    <h1>Details</h1>
+    <h1>Details - {id} </h1> 
+    <p> Hello: {id.product}</p>
+    <ul className="productCard" > 
+      <li>Name: </li>
+      <li>Img: </li>
+      <li>Description: </li>
+      <li>Price: </li>
+      <button> Add to Cart </button>
+    </ul>
     </div>
   );
 }
