a5fe4ddffdab619322f3e437483fa45c2a8b6971
Kassandra Sanchez <kassandrasmedina@gmail.com>
2024-04-04 13:24:10 -0700

    form and labels

--- frontend/client/pages/Register.jsx
+++ frontend/client/pages/Register.jsx
@@ -1,9 +1,28 @@
-import { Link } from 'react-router-dom'
+
 
 const Register = () => {
   return(
     <div>
       <h1>Create Account</h1>
+      <form>
+        <label>
+          <h2>First Name:</h2>
+          <input />
+        </label>
+        <label>
+          <h2>Last Name:</h2>
+          <input />
+        </label>
+        <label>
+          <h2>Email:</h2>
+          <input />
+        </label>
+        <label>
+          <h2>Password:</h2>
+          <input />
+        </label>
+      </form>
+      <button> Register </button>
     </div>
   );
 }
