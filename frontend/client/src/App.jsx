d29d41d36f8101ba65e6742a951767986c0768b6
Kassandra Sanchez <kassandrasmedina@gmail.com>
2024-04-08 16:43:14 -0700

    set token state and passed to components

--- frontend/client/src/App.jsx
+++ frontend/client/src/App.jsx
@@ -1,21 +1,23 @@
 import {  About, Home, Cart, Details, Login, Navbar, Register, Footer, Checkout } from '../pages'
 import './App.css'
 import { Routes, Route} from 'react-router-dom'
+import { useState , useEffect } from 'react';
 
 
 function App() {
+  const [token, setToken] = useState(localStorage.getItem('token'));
 
   return (
     <>
     <div>
-      <Navbar />
+      <Navbar token={token}/>
      <main>
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="login" element={<Login />}/>
-          <Route path="register" element={<Register />}/>
+          <Route path="login" element={<Login token={token} setToken={setToken}/>}/>
+          <Route path="register" element={<Register token={token} setToken={setToken} />}/>
           <Route path="products/:id" element={<Details />}/>
-          <Route path="cart" element={<Cart />}/>
+          <Route path="cart" element={<Cart setToken={setToken}/>}/>
           <Route path="about" element={<About />}/>
           <Route path="checkout" element={<Checkout />} />
         </Routes>
