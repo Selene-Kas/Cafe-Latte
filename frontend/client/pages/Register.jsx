259cd0acbd10b7dee26ed541ae739819a2bff87e
Kassandra Sanchez <kassandrasmedina@gmail.com>
2024-04-08 16:40:58 -0700

    complete register with token

--- frontend/client/pages/Register.jsx
+++ frontend/client/pages/Register.jsx
@@ -1,28 +1,59 @@
+import { useState, useEffect } from "react";
+import { Navigate, useNavigate } from "react-router-dom";
 
+const Register = ({token, setToken}) => {
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const Navigate = useNavigate();
 
-const Register = () => {
+  async function handleSubmit(e) {
+    e.preventDefault();
+    const response = await fetch('http://localhost:3000/api/auth/register', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({firstName, lastName , username, password}),
+    })
+    const { token }= await response.json();
+    localStorage.setItem('token', token);
+    setToken(token);
+  }
+
+  useEffect(()=> {
+    if(token) {
+      Navigate('/');
+    }
+  }, [token, Navigate])
+
   return(
     <div>
-      <h1>Create Account</h1>
-      <form>
+      <h1>CREATE-ACCOUNT</h1>
+      <form onSubmit={handleSubmit}>
         <label>
           <h2>First Name:</h2>
-          <input />
+          <input type="text" value={firstName} placeholder="FIRST NAME..."
+          onChange={(e)=> setFirstName(e.target.value)}/>
         </label>
         <label>
           <h2>Last Name:</h2>
-          <input />
+          <input type="text" value={lastName} placeholder="LAST NAME..."
+          onChange={(e)=> setLastName(e.target.value)}/>
         </label>
         <label>
           <h2>Email:</h2>
-          <input />
+          <input type="email" value={username} placeholder="EMAIL@gmail.com"
+          onChange={(e)=> setUsername(e.target.value)}/>
         </label>
         <label>
           <h2>Password:</h2>
-          <input />
+          <input type="text" value={password} placeholder="PASSWORD..."
+          onChange={(e)=> setPassword(e.target.value)} />
         </label>
+        <button type="submit"> Register </button>
       </form>
-      <button> Register </button>
     </div>
   );
 }
