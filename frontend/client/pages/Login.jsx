2c766ea1d1b7e40c09f85b6199e14219affe9161
Kassandra Sanchez <kassandrasmedina@gmail.com>
2024-04-08 16:40:13 -0700

    complete login with token

--- frontend/client/pages/Login.jsx
+++ frontend/client/pages/Login.jsx
@@ -1,17 +1,43 @@
+import { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 
+const Login = ({token, setToken}) => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const Navigate = useNavigate();
 
-const Login = () => {
+  async function handleSubmit(e) {
+    e.preventDefault();
+    const response = await fetch('http://localhost:3000/api/auth/login', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({username, password}),
+    })
+    const { token }= await response.json();
+    localStorage.setItem('token', token);
+    setToken(token);
+  }
+  useEffect(()=> {
+    if(token) {
+      Navigate('/');
+    }
+  }, [token, Navigate])
+
   return(
     <div>
-      <h1>Login</h1>
-      <form>
+      <h1>LOG-IN</h1>
+      <form onSubmit={handleSubmit}>
         <label>
-            <h2>Email:</h2>
-            <input />
+          <h2>Username:</h2>
+          <input type="email" placeholder="EMAIL@gmail.com" value={username} 
+          onChange={(e)=> setUsername(e.target.value)}/>
         </label>
         <label>
-            <h2>Password:</h2>
-            <input />
+          <h2>Password:</h2>
+          <input type="text" placeholder="PASSWORD..." value={password} 
+          onChange={(e)=> setPassword(e.target.value)} />
         </label>
         <button type="submit"> Login </button>
       </form>
