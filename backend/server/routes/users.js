8a8c426716b579e73165f6a93989d7d626170cba
Kassandra Sanchez <kassandrasmedina@gmail.com>
2024-04-08 19:51:51 -0700

    delete user with middleware

--- backend/server/routes/users.js
+++ backend/server/routes/users.js
@@ -2,7 +2,8 @@ const express = require('express');
 const jwt = require('jsonwebtoken');
 const router = express.Router();
 const { fetchAllUsers,
-    fetchUser
+    fetchUser,
+    deleteUser
 } = require('../db');
 // get all users
 router.get('/', async (req,res, next)=> {
@@ -35,18 +36,26 @@ router.get('/:userId', async (req,res, next)=> {
 // router.delete('/:id', (req, res, next)=> {
 //     res.send(`Delete user with id ${req.params.id}`);
 // })
+router.delete('/:id', requireToken,  async(req,res,next)=> {
+  try{
+    await deleteUser(req.params.id);
+    res.sendStatus(204);
+  } catch(err) {
+    next(err);
+  }
+});
 
 //PROTECTED GET ALL USERS ROUTE (middleware)
-// function requireToken(req, res, next) {
-//     const token = req.headers.authorization;
-//     try {
-//          const user = jwt.verify(token, "secret");
-//          req.user = user;
-//          next();
-//     } catch(err) {
-//         next(err);
-//     }
-// }
+function requireToken(req, res, next) {
+    const token = req.headers.authorization;
+    try {
+         const user = jwt.verify(token, "secret");
+         req.user = user;
+         next();
+    } catch(err) {
+        next(err);
+    }
+}
 
 
 module.exports = router;
