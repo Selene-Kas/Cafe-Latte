8bba03224b74e2482a3f2b1b74bcd2143509fd49
Kassandra Sanchez <kassandrasmedina@gmail.com>
2024-04-09 20:32:56 -0700

    cleaned code

--- frontend/client/pages/api.js
+++ frontend/client/pages/api.js
@@ -14,13 +14,15 @@ export const fetchSingleProduct = async (productId) => {
     console.error(`Oh no, trouble fetching product #${productId}!`, err);
   }
 };
-
+// fetch cart pertaining to user
 export const fetchUserCart= async(userId)=> {
   const response = await fetch(`http://localhost:3000/api/carts/user/${userId}`);
   const data =await response.json();
   console.log(data);
   return(data);
 }
+
+// fetch user(me) and token 
 export const fetchMe= async(token)=> {
   const response = await fetch('http://localhost:3000/api/auth/me', {
     headers: {
