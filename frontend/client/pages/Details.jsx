862b97958aa432b93d6f9798a5cdc7cc4a166cfb
Kassandra Sanchez <kassandrasmedina@gmail.com>
2024-04-09 20:30:31 -0700

    handleClick complete to add product to cart

--- frontend/client/pages/Details.jsx
+++ frontend/client/pages/Details.jsx
@@ -1,9 +1,10 @@
 import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import { fetchSingleProduct } from "./api";
-import cafe from '../src/assets/cafe.png' 
+import cafe from '../src/assets/cafe.png';
+import { fetchUserCart, fetchMe } from "./api";
 
-const Details = () => {
+const Details = ({token}) => {
   const { id }= useParams();
   const [product, setProduct] = useState({});
 
@@ -11,11 +12,39 @@ const Details = () => {
     async function getProductDetails() {
         const productDetails = await fetchSingleProduct(id);
        setProduct(productDetails);
-       console.log(productDetails);
+       //console.log(productDetails);
     }
     getProductDetails();
-  }, [id] )
+  }, [] )
   
+  const handleClick = async() => {
+    async function getMe(){
+      const me = await fetchMe(token);
+      return me;
+      }
+      async function getUserCart(userId){
+      return await fetchUserCart(userId);
+      }
+      const me =await getMe();
+      const cart =await getUserCart(me.id);
+      console.log(me);
+      console.log(cart);
+    fetch(`http://localhost:3000/api/carts/${cart[0].id}/cart_products/${product.id}`, {
+      method: "POST",
+      headers: {
+        'Content-Type': 'application/json',
+        //'Authorization': `Bearer ${localStorage.getItem('token')}`
+      },
+      body: JSON.stringify({
+        qty: 1
+      })
+    }).then(response => response.json())
+      .then(result => {
+        alert('You successfully added product to your cart: '+ product.name);
+      })
+      .catch(console.error);
+  }
+
   return(
     <div>
     <h1> Details <img id="detailsLogo" src={cafe}/></h1> 
@@ -26,7 +55,7 @@ const Details = () => {
       <li>Description: {product.description}</li>
       <li>Price: ${product.product_price}</li>
       <li>Qty: {product.qty_available}</li>
-      <button> Add to Cart </button>
+      <button onClick={handleClick}> Add to Cart </button>
     </ul>
     )}
     </div>
